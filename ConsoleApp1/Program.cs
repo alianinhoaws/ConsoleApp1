4be3bcde0171b56ecd796a29ef3f2d7c26b3b3aa
alianinho <alianinho@gmail.com>
2019-04-14 23:29:28 +0300

    array1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,28 +6,15 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
-            int a, b;
-            a=Convert.ToInt32(Console.ReadLine());
-            switch (a) 
-                {
-                case 1:
-                    b = 45;
-                    Console.Write(b);
-                    break;
-                case 2:
-                    b = 55;
-                    Console.Write(b);
-                    break;
-                case 3:
-                    b = 65;
-                    Console.Write(b);
-                    break;
-                default:
-                    b = 0;      
-                    Console.Write(b);
-                    break;
-
+            int [] arr = new int [5];
+            arr[0] = 54;
+            arr[4] = 33;
+            for (int i=0;i<arr.Length;i++)
+            {
+                Console.Write (arr [i]);
+                Console.Write(" ");
             }
+            
             Console.ReadKey();
             }
         }
