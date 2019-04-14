26d620354632aac8f4510c74a01ccc17610e2c18
alianinho <alianinho@gmail.com>
2019-04-14 22:37:49 +0300

    IF ELSE

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,23 +1,34 @@
 ﻿using System;
 
 namespace ConsoleApp1
-{
+{ 
     class MainClass
     {
         public static void Main(string[] args)
         {
-            int a = 4, b = 3, res;
-
-            res = a - b;
-            res +=2;
-            res--;
-            --res;
-            --res;
-            Console.WriteLine("Res  " + res);
+            double a;
+            bool isTrue = true;
+            a=Convert.ToDouble (Console.ReadLine ());
+            if (a > 10f)
+            {
+                Console.Write("A bigger then 10");
+            }
+            else if (a < 10f && a > 0)
+            {
+                Console.Write("a" + isTrue);
+            }
+            else if (a < 10f && a < 0)
+            {
+                Console.Write("BAD");
+            }
+          
+            Console.WriteLine("Res  " + a);
             Console.ReadKey();
+            }
         }
-    }
 }
+
+
     
  
       
