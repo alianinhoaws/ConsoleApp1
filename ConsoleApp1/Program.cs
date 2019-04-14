637f9c230dcb4e2971107fd19250df97f1803f5a
alianinho <alianinho@gmail.com>
2019-04-15 00:59:29 +0300

    OOP2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,38 +5,39 @@ namespace ConsoleApp1
 {
     class Cars
     {
-        public int wheels = 4;
+        public int wheels;
         private float speed;
         protected bool isWorking = true;
     
-
-    public void SetValues(float speed, bool isWorking)
-    {
-        this.speed = speed;
-        this.isWorking = isWorking;
-
-    }
         public void GetValues()
         {
             Console.WriteLine("Car speed " + this.speed + ", car is working "
                 + this.isWorking + " Wheels " + this.wheels);
         }
 
+        public Cars (int wheels, float speed, bool isWorking)
+        {
+            this.wheels = wheels;
+            this.speed = speed;
+            this.isWorking = isWorking;
+
+        }
+
+        public Cars() // перегрузка для снятия ощибки
+        {
+
+        }
 
         class MainClass
         {
             public static void Main (string[] args)
             {
-                Cars Audi = new Cars();
-
-                Audi.SetValues(223.3f, true);
-                Audi.GetValues ();
-                Cars Volvo = new Cars();
-             
-                Volvo.wheels = 6;
-                Volvo.SetValues(180.3f, false);
+                Cars Audi = new Cars(4, 223.4f, true);
+                Audi.GetValues();
+                Cars Volvo = new Cars(6,180.4f,false);
                 Volvo.GetValues();
-               
+                Cars Renault = new Cars();
+                Renault.GetValues();
                 Console.ReadKey();
             }
         }
